4848bb0f7941c2bc3f07c915faf122ac737feadc
Diego M Riano-Pachon <diriano@gmail.com>
2024-08-22 15:43:12 -0300

    Adding DB schema as sql file

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS sequence;
+DROP TABLE IF EXISTS sequenceSet;
+DROP TABLE IF EXISTS sequence2sequenceSet;
+
+CREATE TABLE sequence(
+  ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  sequenceIdentifier VARCHAR(255) NOT NULL,
+  sequenceVersion INTEGER NOT NULL,
+  sequenceType VARCHAR(255) CHECK ( sequenceType IN ('DNA', 'Protein') ) NOT NULL DEFAULT 'DNA',
+  sequence TEXT NOT NULL,
+  CONSTRAINT uniqueSequenceIdVersionType,
+  UNIQUE(sequenceIdentifier,sequenceVersion,sequenceType) ON CONFLICT ABORT
+);
+
+CREATE INDEX idx_sequenceIdentifier ON sequence(sequenceIdentifier);
+
+CREATE TABLE sequenceSet (
+  ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  nameSet VARCHAR(255) NOT NULL UNIQUE,
+);
+
+CREATE INDEX idx_nameSet ON sequenceSet(nameSet);
+
+CREATE TABLE sequence2sequenceSet (
+  sequenceID INTEGER NOT NULL,
+  sequenceSetID INTEGER NOT NULL
+  PRIMARY KEY(sequenceID,sequenceSetID),
+  FOREIGN KEY(sequenceID) REFERENCES sequence(ID),
+  FOREIGN KEY (sequenceSetID) REFERENCES sequenceSet(ID)
+);
