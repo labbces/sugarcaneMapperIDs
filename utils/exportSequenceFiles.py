9326fbcf7c4b8892237dd45c1deba743a45d512e
Diego M Riano-Pachon <diriano@gmail.com>
2025-05-06 13:08:49 -0300

    improving templae

--- utils/exportSequenceFiles.py
+++ utils/exportSequenceFiles.py
@@ -1,11 +1,13 @@
 # exportSequenceFiles.py
-
-from model import Sequence, Sequence2Set, SequenceSet, panTranscriptomeGroup
-import argparse
+import os
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from db.model import Sequence, Sequence2Set, SequenceSet, panTranscriptomeGroup
+import argparse
+
 #initialize the parser 
-parser = argparse.ArgumentParser(description='Populate the database with sequences and orthogroups')
+parser = argparse.ArgumentParser(description='extract sequences from the database and write them to files')
 parser.add_argument('--prefix', type=str, required=True, help='string to use as prefix of output filenames')
 args = parser.parse_args()
 
@@ -21,7 +23,9 @@ for set in sets:
     for ext in sequenceClassExt:
         with open(f"{basename}_{ext}.{sequenceClassExt[ext]}", 'w') as f, open(f"{basename}_representative_{ext}.{sequenceClassExt[ext]}", 'w') as f2:
             for seq in Sequence.select().join(Sequence2Set).join(SequenceSet).where(SequenceSet.nameSet==set.nameSet, Sequence.sequenceClass==ext, Sequence.sequenceVersion==1):
+                print(seq.ID)
                 og=panTranscriptomeGroup.select(panTranscriptomeGroup.groupID,panTranscriptomeGroup.representative).where(panTranscriptomeGroup.sequenceID==seq.ID).first()
-                f.write(f">{seq.sequenceIdentifier} OG={og.groupID} representative={og.representative}\n{seq.sequence}\n")
-                if og.representative == 1:
-                    f2.write(f">{seq.sequenceIdentifier} OG={og.groupID} representative={og.representative}\n{seq.sequence}\n")
+                # print(og)
+                # f.write(f">{seq.sequenceIdentifier} OG={og.groupID} representative={og.representative}\n{seq.sequence}\n")
+                # if og.representative == 1:
+                    # f2.write(f">{seq.sequenceIdentifier} OG={og.groupID} representative={og.representative}\n{seq.sequence}\n")
